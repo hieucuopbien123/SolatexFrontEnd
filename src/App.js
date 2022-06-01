d0a27f2eb27a4c019f3ac969ffdf53349535a72a
hieucuopbien123 <70965655+hieucuopbien123@users.noreply.github.com>
2022-06-01 17:28:46 +0700

    feat: add simple app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NewTask from "src/pages/NewTask";
+import ToDoList from "./pages/ToDoList";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState(JSON.parse(window.localStorage.getItem("task")));
+    return (
+        <div className="flexContainer">
+            <div className="halfContainer">
+                <NewTask data={data} setData={setData} />
+            </div>
+            <div style={{ borderRight: "1px solid black" }}></div>
+            <div className="flexContainer halfContainer halfContainer_custom">
+                <ToDoList data={data} setData={setData} />
+            </div>
+        </div>
+    );
+};
 
 export default App;
